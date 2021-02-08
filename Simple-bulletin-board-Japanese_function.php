ddd6ea072fb15d705b43b97ebfe22e606b511d04
Oni-Tenzikunezumi <78721329+Oni-Tenzikunezumi@users.noreply.github.com>
2021-02-08 21:15:05 +0900

    Add files via upload

---
+++ Simple-bulletin-board-Japanese_function.php
@@ -0,0 +1,246 @@
+<?php
+$LogTable = "logfile";//ログを記録するテーブル
+$D_Prevent = "rand";//二重送信防止用のテーブル
+
+//データベースへ接続
+$dsn = 'mysql:dbname=********;host=********';//Data Source Name
+$user = '*******';//ユーザー名
+$password = '********';
+$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));//PHP Data Objects
+
+//コメント用のテーブルの作成
+$sql = "CREATE TABLE IF NOT EXISTS {$LogTable} (id INT AUTO_INCREMENT PRIMARY KEY, name char(32), dat char(32), editdat char(32), comment TEXT, pass char(32));";
+$state = $pdo->query($sql);//コメントid,ユーザーname,投稿した日付,最後に編集した日付,コメント内容,パスワード
+
+
+//コメントidのリンクから、GETでidを取得(show関数)
+  if(isset($_GET["getid"])&&is_numeric($_GET["getid"])){
+    $editid = $_GET["getid"];
+  }else{//初回実行時などは空欄
+    $editid = "";
+  }
+
+/*--------------------ここから主な処理の開始--------------------*/
+//フォームの表示変更
+  /*$configの値に対して分岐する。$configはeditdataから変数を受け取る。
+    フォーム欄の表示内容、実行ボタンの内容$submit_valueの値を決める。
+    $configに想定していない値が入っていた場合は不正なアクセスがあったものとして扱う
+    処理中に発生したメッセージは、全て配列$messageに入れ、フォーム欄の下で表示する。*/
+
+    if(isset($_POST["config"])){//初回実行時、リンクのクリック時はとばす
+      $config = $_POST["config"];//値の受け取り
+      $message = array();//値の初期化
+
+      if($config=="投稿"||$config=="キャンセル"){//投稿、キャンセルボタンを押した場合、フォームには何も表示しない
+        //ただの投稿に設定、その他の設定は使用しない
+        $submit_value = "投稿";
+        $editid = "";
+
+      }elseif($config=="編集"||$config=="削除"){//編集・削除の申請があった場合、フォームには何も表示しない
+        //フォーム表示の変更
+        //値の取得
+        $inputid = $_POST["editid"];//編集番号
+        $inputpass = $_POST["inputpass"];//入力されたパスワード
+        $pass_auth = pass_auth($pdo, $LogTable, $inputpass, $inputid);//パスワード判定
+
+        if($pass_auth["result"]){//パスワードが合致した場合
+          $message[] = $pass_auth["message"];//メッセージを追加
+          //値の取得
+          $sql = $pdo->prepare("SELECT * FROM logfile WHERE id=:editid");
+          $sql->bindParam(":editid", $inputid, PDO::PARAM_INT);
+          $sql->execute();
+          $result = $sql->fetch();
+          //値の差し替え、投稿内容を配列に入れる。
+          $display = [ "id" => $result["id"],
+                     "name" => $result["name"],
+                     "pass" => $result["pass"],
+                     "comment" => $result["comment"], ];
+          $submit_value = $config;
+
+        }else{//パスワードが合致しなかった場合
+          $message[] =  $pass_auth["message"];//メッセージを追加
+          $message[] =  "もう一度入力してください。<br>";
+          $submit_value = "投稿";
+        }
+
+      }else{//想定していない値の場合
+        $message[] =  "不正なアクセスが発生しました。(config)";
+        $submit_value = "投稿";
+        $editid = "";
+      }
+    }else $submit_value = "投稿";
+
+
+/*--------------------ユーザー定義関数--------------------*/
+
+//二重登録防止用の関数
+function reload($pdo, $table){//PDOインスタンス、テーブル名
+  /*乱数の値はフォーム欄のPOSTにname=reloadで入れる(hidden)
+    乱数を作りテーブルに挿入し、その値の変化でリロードの有無を判別する。
+    リロードの有無を論理型(reload)で、次の乱数の値を整数型(rand)で与え、連想配列にして返す。
+    <input type="hidden" name="reload" value="<?=array[rand]?>">*/
+
+  //二重登録対策のテーブルの作成
+  $sql = "CREATE TABLE IF NOT EXISTS {$table} (rand INT);";
+  $state = $pdo->query($sql);
+
+  //値の取得
+  $sql = $pdo->prepare("SELECT rand FROM ".$table);
+  $sql -> execute();
+  $results = $sql -> fetch();
+  $reload = false;
+
+  //リロードの検出
+  if((isset($_POST["reload"]))&&(isset($results["rand"]))){//初回起動時は実行しない
+    if($_POST["reload"]==$results["rand"]){//リロードされていない場合
+      $reload = false;
+    }else $reload = true;//リロードされた場合
+  }
+
+    //テーブル内をリセット
+    $sql = $pdo->prepare("DELETE FROM ".$table);
+    $sql -> execute();
+    //新しい値を挿入
+    $rand = rand();
+    $sql = $pdo->prepare("INSERT INTO {$table} (rand) VALUES (:rand)");
+    $sql -> bindParam(":rand", $rand, PDO::PARAM_INT);
+    $sql -> execute();
+
+    //値を配列して返す
+    $array = [ "reload" => $reload, "rand" => $rand,];
+    return $array;
+}
+
+
+//パスワードの正誤判定用関数
+function pass_auth($pdo, $table, $pass, $editid){//PDOインスタンス、テーブル名、入力されたパスワード、申請されたid
+  /*idとパスワード内容から正誤判定
+  *引数以外のメインでの処理なし
+    判定結果とメッセージを論理型と文字列の配列に入れて返す
+    この関数は他の関数からのみ呼び出される*/
+  $sql = $pdo->prepare("SELECT * FROM {$table} WHERE id=:editid");//idの一覧
+  $sql->bindParam(":editid", $editid, PDO::PARAM_INT);
+  $sql->execute();
+  $data = $sql->fetch();
+  if($data["id"]==$editid){//idが存在するかどうかの判別
+    if($data["pass"]==$pass){//パスワードの正誤判定
+      $array = [ "result" => true, "message" => "パスワード認証に成功しました。<br>",];//パスワードが合致した場合
+    }else $array = [ "result" => false, "message" => "パスワードが異なります。<br>",];//異なるパスワードだった場合
+  }else $array = [ "result" => false, "message" => "存在しないidです。<br>",];//存在しないidだった場合
+
+  return $array;
+}
+
+
+//データレコードの変更用関数
+function editdata($pdo, $table){//PDOインスタンス、テーブル名
+  /*$_POST["submit"]の値にによって分岐する
+    入力された値に対してバリデーションを行う
+    空欄があった場合は処理をキャンセルし、もう一度入力させる
+    *申請時のパスワードを実行時にPOST,hidden,inputpassで送信する
+    編集・削除の前にもパスワードの判定を行う。パスワードは実行時にPOST,hidden,inputpassで送信し、この値が合致しなかった場合、不正な値が入ったとして処理する
+    この関数に返り値はない*/
+
+  if(isset($_POST["submit"])){//初回の実行時,リンククリック時以外は実行
+    $submit = $_POST["submit"];//データレコードの変更設定
+    $date = date("d/m/y H:i:s(D)");//日付の取得
+    $input = false;//正しく入力したかを確認するパラメータの初期化
+
+    //$submitの値に対する分岐
+    if($submit!="申請"){//申請時以外の時、入力した値を受け取り、分岐する
+      //値のバリデーション(全角スペースを削除)
+      if(trim($_POST["name"], " \n\r\t\v\0　")==""){//名前の取得(空欄の時名無しに変更)
+          $name = "名無しさん";
+      }else $name = $_POST["name"];
+
+      if(trim($_POST["pass"], " \n\r\t\v\0　")==""){//パスワード(空欄の時キャンセル)
+        echo "パスワードが入力されていません。<br>";
+        $input = true;
+      }else $pass = $_POST["pass"];//前後のスペース等は消去
+
+      if(trim($_POST["comment"], " \n\r\t\v\0　")==""){//コメント内容(空欄の時キャンセル)
+        echo "コメントが入力されていません。<br>";
+        $input = true;
+      }else $comment = rtrim($_POST["comment"], " \n\r\t\v\0　");
+
+      if($input){//正しく入力されていなかった場合、最初から入力させる。
+        echo "もう一度正しく入力してください。<br>";//エラーメッセージを表示しそれ以外の処理は行わない
+
+      }else{//正しく入力されていた場合
+        //データレコードの変更
+        if($submit=="投稿"){//投稿ボタンを押したとき、ただの書き込み
+          $sql = $pdo -> prepare("INSERT INTO {$table} (name, dat, comment, pass) VALUES (:name, :dat, :comment, :pass)");
+          $sql->bindParam(':name', $name, PDO::PARAM_STR);
+          $sql->bindParam(':dat', $date, PDO::PARAM_STR);
+          $sql->bindParam(':comment', $comment, PDO::PARAM_STR);
+          $sql->bindParam(':pass', $pass, PDO::PARAM_STR);
+          $sql->execute();
+          echo "コメントを投稿しました。<br>";
+
+        }elseif($submit=="編集"){//編集ボタンを押したとき、内容の更新
+          $editid = $_POST["editid"];//編集番号
+          $inputpass = $_POST["inputpass"];//入力されたパスワード
+          $pass_auth = pass_auth($pdo, $table, $inputpass, $editid);//パスワード判定
+          if($pass_auth["result"]){//結果で分岐
+            $sql = $pdo->prepare("UPDATE {$table} SET name=:name, editdat=:editdat, comment=:comment, pass=:pass WHERE id=:editid;");
+            $sql->bindParam(':name', $name, PDO::PARAM_STR);
+            $sql->bindParam(':editdat', $date, PDO::PARAM_STR);
+            $sql->bindParam(':comment', $comment, PDO::PARAM_STR);
+            $sql->bindParam(':pass', $pass, PDO::PARAM_STR);
+            $sql->bindParam(':editid', $editid, PDO::PARAM_INT);
+            $sql->execute();
+            echo "{$editid}番のコメントを編集しました。<br>";
+          }else echo "不正な値が入力されました。(pass)<br>";
+
+        }elseif($submit=="削除"){//削除ボタンを押したとき、内容の削除
+          $editid = $_POST["editid"];//編集番号
+          $inputpass = $_POST["inputpass"];//入力されたパスワード
+          $pass_auth = pass_auth($pdo, $table, $inputpass, $editid);//パスワード判定
+          if($pass_auth["result"]){//結果で分岐
+            $editid = $_POST["editid"];//編集番号
+            $sql = $pdo->prepare("DELETE FROM {$table} where id=:editid;");
+            $sql->bindParam(':editid', $editid, PDO::PARAM_INT);
+            $sql->execute();
+            echo "{$editid}番のコメントを削除しました。<br>";
+          }else echo "不正な値が入力されました。(pass)<br>";
+
+        }else echo "不正なアクセスが発生しました。(submit)<br>";//&submitの値が想定外の物であった場合
+      }//データレコードの変更終了
+    }//編集・削除の申請時、何もしない,$submitの分岐処理終了
+  }//初回実行時等は何もしない
+}
+
+
+//表示用関数
+function show($pdo, $table, $filename){//PDOインスタンス、テーブル名、実行しているファイル名の指定
+  /*リンククリックの値はGETのgetidに入れる
+    idのみリンクで表示、名前とコメントはエスケープ処理を行う
+    更にコメントは改行を入れて表示させる
+    *変数以外でメイン側から行う処理なし
+    返り値なし*/
+
+  //値の取得
+  $sql = $pdo->prepare("SELECT * FROM ".$table);
+  $sql->bindParam(":table", $table, PDO::PARAM_STR);
+  $sql->execute();
+  $result = $sql->fetchall();
+  $logdata = array_reverse($result);//配列を逆順にし、表示の際最新のコメントがフォームの直後に来るようにする
+
+  //値の表示
+  echo "<hr><hr>";
+  foreach($logdata as $log){//コメント番号をハイパーリンクで表示し、クリックでコメント番号を取得できるようにする?>
+    <a href="<?=$filename?>?getid=<?=$log["id"]?>"><?=$log["id"]?></a>
+    <?php
+    echo ":".htmlspecialchars($log["name"])."<br>";//idと名前を一行で表示
+
+    if(isset($log["editdat"])){//編集履歴があるときのみ編集時間を表示
+        echo "Post:".$log["dat"]."=>Edit:".$log["editdat"]."<br>";
+      }else{
+        echo "Post:".$log["dat"]."<br>";//日付は一行で表示する
+      }
+
+    echo nl2br(htmlspecialchars($log["comment"]))."<br>";//コメントは改行を入れて表示
+    echo "<hr>";//コメントごとにラインを挿入
+  }
+}
+?>
---
+++ Simple-bulletin-board-Japanese_main.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_5-1</title>
+</head>
+
+<?php
+include "Simple-bulletin-board-Japanese_function.php";
+//ユーザー定義関数の呼び出し
+$reload = reload($pdo, $D_Prevent);//リロードの検出
+?>
+
+<html>
+<body>
+<div style="text-align:center">
+
+<form action="" method="post">
+  <input type="hidden" name="reload" value="<?=$reload["rand"]?>"><!--リロード判定用パラメータ-->
+
+  <?php if($submit_value=="投稿")://投稿を行う場合 ?>
+    <?php if($editid=="")://idの選択がない場合?>
+            <!--投稿用フォームの表示-->
+            mission5-1 掲示板<br>
+            <input type="text" name="name" placeholder="名前">
+            <input type="password" name="pass" placeholder="password" required>
+            <br>
+            <textarea name="comment" rows="4" cols="50" placeholder="コメントを入力…"></textarea>
+            <br>
+            <input type="hidden" name="config" value="投稿"><!--表示設定-->
+            <input type="submit" name="submit" value="投稿"><!--投稿ボタン-->
+
+            <input type="reset" name="reset"><!--リセットボタン-->
+            <br>編集、削除はコメント番号を選択してください。<br>
+
+    <?php else://編集、削除の申請を行う場合?>
+            <!--申請用フォームの表示-->
+            パスワードを入力してください。<br>
+            <input type="password" name="inputpass" placeholder="password">
+            <br>
+            <input type="hidden" name="submit" value="申請"><!--次の編集は行わない-->
+            <input type="number" name="editid" value="<?php echo $editid?>" style="width:40px" placeholder="コメント番号" min=0>
+            <input type="submit" name="config" value="編集"><!--編集ボタン-->
+
+            <input type="submit" name="config" value="削除"><!--削除ボタン-->
+
+            <input type="submit" name="config" value="キャンセル"><!--キャンセルボタン-->
+    <?php endif; ?>
+
+  <?php elseif($submit_value=="編集")://編集を行う場合?>
+          <!--編集用フォームの表示-->
+          編集中。<br>
+          <input type="text" name="name" placeholder="名前" value="<?=$display["name"]?>" >
+          <input type="password" name="pass" placeholder="password" value="<?=$display["pass"]?>" required>
+          <br>
+          <textarea name="comment" rows="4" cols="50" placeholder="コメントを入力…" ><?=$display["comment"]?></textarea>
+          <br>
+          <input type="number" name="editid" value="<?=$display["id"]?>" style="width:50px" readonly><!--コメント番号-->
+          <input type="hidden" name="inputpass" value="<?=$inputpass?>">
+          <input type="hidden" name="config" value="投稿"><!--configを再設定-->
+          <input type="submit" name="submit" value="編集"><!--編集ボタン-->
+
+          <input type="submit" name="config" value="キャンセル"><!--キャンセルボタン-->
+
+          <input type="reset" name="reset"><!--リセットボタン-->
+
+  <?php elseif($submit_value=="削除")://削除を行う場合?>
+          <!--削除用フォームの表示-->
+          このコメントを削除しますか？<br>
+          <input type="text" name="name" value="<?=$display["name"]?>" readonly>
+          <input type="password" name="pass" value="<?=$display["pass"]?>" readonly>
+          <br>
+          <textarea name="comment" rows="4" cols="50" readonly><?=$display["comment"]?></textarea>
+          <br>
+          <input type="number" name="editid" value="<?=$display["id"]?>" style="width:50px" readonly><!--コメント番号-->
+          <input type="hidden" name="inputpass" value="<?=$inputpass?>">
+          <input type="hidden" name="config" value="投稿"><!--configを再設定-->
+          <input type="submit" name="submit" value="削除"><!--削除ボタン-->
+
+          <input type="submit" name="config" value="キャンセル"><!--キャンセルボタン-->
+  <?php endif;?>
+</form>
+
+</div>
+<hr>
+  <?php
+  //メッセージの表示
+  if(isset($message)){
+    foreach($message as $message) echo $message;
+  }
+  //ユーザー定義関数の呼び出し
+  //リロードの判定結果で分岐
+  if(!$reload["reload"]) editdata($pdo, $LogTable);//リロードされていない場合、データレコードの変更
+  show($pdo, $LogTable, basename(__FILE__));//表示,実行しているファイル名の取得
+  ?>
+</body>
+</html>
